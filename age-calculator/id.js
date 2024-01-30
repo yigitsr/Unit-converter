9bc090ba6f38656f042c90d4c11b35ef1089830c
Yigit S <55548182+yigitsr@users.noreply.github.com>
2024-01-30 11:49:21 +0300

    Add files via upload

---
+++ age-calculator/id.js
@@ -0,0 +1,19 @@
+const database = [
+    {
+        tc: "1111",
+        name: "yigit" 
+    },
+
+    {
+        tc: "2222",
+        name: "ezgi" 
+    }, 
+    {
+        tc: "3333",
+        name: "sinem" 
+    },
+    {
+        tc: "4444",
+        name: "gizem" 
+    }
+]
